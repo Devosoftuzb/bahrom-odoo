5cc8da5f231777267022f876303e20465e66f4d5
Bahrom <najmiddinovbahrom402@gmail.com>
2024-10-08 12:59:50 +0500

    MailPro search order status added

--- mailpro/custom_addons/post/controllers/search_order.py
+++ mailpro/custom_addons/post/controllers/search_order.py
@@ -5,14 +5,19 @@ from odoo.http import request
 class SearchOrder(http.Controller):
     @http.route('/search-order/', type='http', auth='public', website=True)
     def search_order(self):
-        order_status = self.env['stock.picking'].search((
-            [('priority', '=', request.params['priority'])],
-        ), limit=1)
+        # Initialize variables
+        order_status = None
+        track_id = request.params.get('track_id')
 
+        # Check if the tracking ID is provided
+        if track_id:
+            # Search for stock.picking records with the given track ID
+            order_status = request.env['stock.picking'].search([('name', '=', track_id)], limit=1)
+
+        # If no records were found, order_status will be an empty recordset
         context = {
-            'results': order_status.read(['name', 'state', 'priority']),
-            'search': request.params,
-            'fuzzy_search': [],
+            'results': order_status,  # Pass the recordset directly
+            'search': track_id,        # Set the search value for the template
         }
 
         return request.render('post.search_order_template', context)
