ccbd0723eb4b75f04c57a5533b29f1e6b080f822
Bahrom <najmiddinovbahrom402@gmail.com>
2024-10-09 15:25:39 +0500

    Updated

--- mailpro/custom_addons/post/controllers/search_order.py
+++ mailpro/custom_addons/post/controllers/search_order.py
@@ -5,7 +5,6 @@ from odoo.http import request
 class SearchOrder(http.Controller):
     @http.route('/search-order/', type='http', auth='public', website=True)
     def search_order(self):
-        # Initialize variables
         order_status = None
         track_id = request.params.get('track_id')
 
@@ -14,10 +13,9 @@ class SearchOrder(http.Controller):
             # Search for stock.picking records with the given track ID
             order_status = request.env['stock.picking'].search([('name', '=', track_id)], limit=1)
 
-        # If no records were found, order_status will be an empty recordset
         context = {
-            'results': order_status,  # Pass the recordset directly
-            'search': track_id,        # Set the search value for the template
+            'results': order_status,
+            'search': track_id,
         }
 
         return request.render('post.search_order_template', context)
